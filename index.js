ee049a22908ccbfff3e635871f845a24698f9186
Erik Gustafson <erik.d.gustafson@gmail.com>
2018-02-02 16:01:54 -0600

    Edit URL to point to erikdgustafson.com

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ import './style';
 import { Component } from 'preact';
 import axios from 'axios';
 
+// don't get excited, not a real token
 axios.defaults.headers.common['Authorization'] = 'Bearer ' + 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ';
 
 export default class App extends Component {
 
    getRequestJWT = () => {
-      axios.get('http://localhost:4040/preact-pil-jwt/!getRequestJWT')
+      axios.get('https://erikdgustafson.com/preact-pil-jwt/!getRequestJWT')
          .then( r => console.log(r) )
    }
 
